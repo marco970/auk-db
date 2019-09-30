74af78905a225ed7efa8b5009f6a4b5075c95b15
Marcin <m.kuciak@gmail.com>
2019-09-30 20:08:33 +0200

    next

--- src/pl/auk/front/MainWindow.java
+++ src/pl/auk/front/MainWindow.java
@@ -1,8 +1,12 @@
 package pl.auk.front;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+
+import net.miginfocom.swing.MigLayout;
 
 public class MainWindow extends JFrame {
 	
@@ -27,15 +31,49 @@ public class MainWindow extends JFrame {
 		
 //		JFrame frame = new JFrame("Aukcja - okno g³ówne");
 		
-		int width = 100;
-		int height=	200;	
+		int width = 500;
+		int height=	400;	
 		setSize(width, height);
 //		frame.setIconImage(img.getImage());
+		
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new MigLayout("[grow]", "[grow]", "[grow]"));
+		
+		/**
+		 * do pojedyñczej komórki bêdzie dodawany JPanel
+		 * bêdzie on zawiera³ Listê obieków,
+		 * obiekt bêdzie wierszem z ró¿nymi polami...
+		 * a mo¿e tabelê? - to chyba dobry pomys³
+		 * 
+		 * property to bêdzie Lista<JTable> 
+		 * 
+		 * czyli w komórce panel
+		 * w panelu JTable
+		 * w ostatnim panelu tak¿e button (a mo¿e w kolejen komórce wiersza)
+		 * 	- raczej kilka buttonów:
+		 * 		wyœlij maile,
+		 * 		cofnij
+		 * 		dodaj komunikat
+		 * 		itd
+		 * 
+		 * testowo, zanim zrobê BD i transakcje, bêdzie potrzebna klasa tworzaca potrzebne obiekty
+		 * 
+		 * 
+		 * 		
+		 * 
+		 * 
+		 * 
+		 */
+		
+		setContentPane(contentPane);
 		
 		JScrollPane scroll = new JScrollPane();
 		
 		add(scroll);
 		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true);
 	}
 
 }
--- src/pl/auk/start/Main.java
+++ src/pl/auk/start/Main.java
@@ -1,9 +1,12 @@
 package pl.auk.start;
 
+import pl.auk.front.MainWindow;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		new MainWindow();
 
 	}
 
