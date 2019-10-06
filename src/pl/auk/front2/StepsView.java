47feb603709037ffdc52bf00ada41b5cc278c720
Marcin <m.kuciak@gmail.com>
2019-10-06 23:13:29 +0200

    pierdzielenie się z frontendem

--- src/pl/auk/front2/StepsView.java
+++ src/pl/auk/front2/StepsView.java
@@ -2,6 +2,7 @@ package pl.auk.front2;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Vector;
 
@@ -27,68 +28,54 @@ public class StepsView extends JPanel {
 	
 	public void drawPanel(List<List<OfferEnti>> stepList)	{
 		
-//		System.out.println("StepsView - drawPanel");
-		
-	    Vector<String> columnNames = new Vector<String>();
-	    columnNames.addElement("oferent");
-	    columnNames.addElement("cena");
-		
 		int stepNr = stepList.size();
-//		System.out.println("drawPanel stepNr "+ stepNr); 
 		String migRow = "10";
 		for (int i = stepNr; i<1; i--)	{
 			migRow=migRow+"[grow]20";
 		}
 		this.setLayout(new MigLayout(
 				"",
-				"100[grow]10[grow]10",
+				"30[grow]10[grow]100",
 				migRow
 				));
-		for (int i = stepNr; i>1; i--)	{
-			System.out.println("testloop "+i);
-		}
+
 		
 		for (int i = stepNr; i>0; i--)	{	//tyle tabel ile kroków
-			Vector<Vector> dane = new Vector<>();
 			System.out.println("first loop "+i);
+			
+//			add(new JLabel("step nr "+i), "cell 0 "+(stepNr-i));
+			String start;
 			if (i==stepNr)	{
 				add(new JLabel("buttons"), "cell 1 0");
+				start = "<html><body><table style=\"width: 100%; height: 0; border: 1px solid #000000; background-color: #FFFFFF;\" cellpadding=\"1\" cellspacing=\"0\">";
 			}
-//			for (OfferEnti el: stepList.get(i-1)) {
-//				add(new JLabel(el.toString()), "cell 0 "+i);
-//				System.out.println(el.toString());
-//			}
-			String start = "<html><body><table>";
-			String naglowki = "<tr><td style=\"width: 100px\"><b>Oferent</b></td><td style=\"width: 100px\"><b>Cena</b></td></tr>";
+			else {
+				start = "<html><body><table style=\"width: 100%; height: 0; border: 1px solid #000000;\" cellpadding=\"1\" cellspacing=\"0\">";
+				
+			}
+			String stepHeader = "<tr><td colspan=\"4\" style=\"height: 30px; vertical-align: top;\"><p><b>step nr "+(i-1)+"</b><br />minimalne post¹pienie "+100+"<br /></p></td></tr>";
+			String naglowki = "<tr><td style=\"width: 100px;\"><b>Oferent</b>"
+					+ "</td><td style=\"width: 60px;\"><b>Cena</b></td>"
+					+ "</td><td style=\"width: 100px\"><b>Cena z domiarem</b></td>"
+					+ "</td><td style=\"width: 100px\"><b>Pozycja</b></td></tr>";
 			String end = "</table></body></html>";
 			String middle = "";
+			
+		      DecimalFormat myFormatter = new DecimalFormat("CUR ###,###");
+		      
+			
 			for (int j=0; j<stepList.get(i-1).size(); j++)	{
+				String output = myFormatter.format(stepList.get(i-1).get(j).getCena());
 				System.out.println("--> "+stepList.get(i-1).get(j));
 				middle = middle +"<tr><td>"+stepList.get(i-1).get(j).getOferent().toString()+"</td>"
-						+ "<td>"+stepList.get(i-1).get(j).getCena()+"</td></tr>";
-				
-				
+						+ "<td style=\"text-align: right;\">"+output+"</td></tr>";
 			}
-			
-//			for (int j=0; j<stepList.get(i-1).size(); j++)	{
-//				Vector<Object> row = new Vector<>();
-//				row.addElement(stepList.get(i-1).get(j).getOferent());
-//				row.addElement(stepList.get(i-1).get(j).getCena());
-//				dane.addElement(row);
-//				System.out.println("--> "+stepList.get(i-1).get(j));
-//			}
-//			JTable table = new JTable(dane,columnNames);
-			JLabel html = new JLabel(start+naglowki+middle+end);
+
+			JLabel html = new JLabel(start+stepHeader+naglowki+middle+end);
 			html.setFont(new Font("SanSerif", Font.PLAIN, 12));
 //			html.setForeground(new Color(0xffffdd));
-			add(html, "cell 0 "+i);
-			
-			
+			add(html, "cell 0 "+(stepNr-i));	
 		}
-		
-//		add(new JLabel("drawPanel - test"));
-		
-		
 	}
 	
 	
