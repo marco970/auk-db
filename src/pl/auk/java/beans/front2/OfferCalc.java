324775227d3e714fa5781afad6237ef63a410fcf
marco970 <m.kuciak@gmail.com>
2019-10-18 15:30:25 +0200

    pójdzie

--- src/pl/auk/java/beans/front2/OfferCalc.java
+++ src/pl/auk/java/beans/front2/OfferCalc.java
@@ -28,7 +28,7 @@ public class OfferCalc {
 				}
 			}
 
-			System.out.println("j "+j+" pos "+pos);
+//			System.out.println("j "+j+" pos "+pos);
 //			System.out.println(step.get(j).toString()+" pos2 "+pos);
 			OfferEnti oe = new OfferEnti(
 					step.get(j).getStepNr(),
