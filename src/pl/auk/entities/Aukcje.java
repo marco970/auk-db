65f5f942be985c01828bf4056cc9c041cef4691b
marci <marci@192.168.1.148>
2019-11-17 19:38:07 +0100

    doszła waluta

--- src/pl/auk/entities/Aukcje.java
+++ src/pl/auk/entities/Aukcje.java
@@ -34,6 +34,9 @@ public class Aukcje {
 	@Column(name="data-start")
 	private String dataStartAk;
 	
+	@Column(name="waluta")
+	private String waluta;
+	
 	@OneToMany(mappedBy="aukcje",
 			   cascade= {CascadeType.PERSIST, CascadeType.MERGE,
 						 CascadeType.DETACH, CascadeType.REFRESH})
@@ -54,11 +57,13 @@ public class Aukcje {
 	public Aukcje()	{
 	}
 
-	public Aukcje(String nazwaAuk, String opisAuk, String dataCreateAuk, String dataStartAk) {
+	public Aukcje(String nazwaAuk, String opisAuk, String dataCreateAuk, String dataStartAk, String waluta) {
 		this.nazwaAuk = nazwaAuk;
 		this.opisAuk = opisAuk;
 		this.dataCreateAuk = dataCreateAuk;
 		this.dataStartAk = dataStartAk;
+		this.waluta = waluta;
+		
 	}
 
 
@@ -110,7 +115,17 @@ public class Aukcje {
 	public void setDataStartAk(String dataStartAk) {
 		this.dataStartAk = dataStartAk;
 	}
+	
+	
 
+
+	public String getWaluta() {
+		return waluta;
+	}
+
+	public void setWaluta(String waluta) {
+		this.waluta = waluta;
+	}
 
 	public List<Bidders> getBidders() {
 		return bidders;
@@ -161,11 +176,10 @@ public class Aukcje {
 		bids.add(bid);
 		bid.setAukcja(this);
 	}
-	
-	
-	
-//	tString - na koniec
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "Aukcje [idAukcja=" + idAukcja + ", nazwaAuk=" + nazwaAuk + ", opisAuk=" + opisAuk + ", dataCreateAuk="
+				+ dataCreateAuk + ", dataStartAk=" + dataStartAk + ", waluta=" + waluta + "]";
+	}	
 }
